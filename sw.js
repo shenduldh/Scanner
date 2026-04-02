92f155e1d7e44fc8e88ce4ca19d8bed98f7d8ef8
liudh <liudh@gdshuyeit.com>
2026-04-02 15:52:48 +0800

    update at 202604021552

--- sw.js
+++ sw.js
@@ -1,20 +1,22 @@
-const CACHE_NAME = "offline-v1";
-const OFFLINE_URL = "index.html" // 离线时显示的页面
+const CACHE_NAME = "PWA-v1"
+const ASSETS = [
+    "./index.html",
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-192.png",
+    "sw.js",
+]
 
-
-self.addEventListener("install", (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.add(OFFLINE_URL))
-    )
+self.addEventListener("install", event => {
+    event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)))
     self.skipWaiting()
 })
 
-self.addEventListener("fetch", (event) => {
-    if (event.request.mode === "navigate") {
-        event.respondWith(
-            fetch(event.request).catch(() => {
-                return caches.match(OFFLINE_URL)
-            })
-        );
-    }
-});
+self.addEventListener("activate", event => {
+    caches.keys().then(cacheNames => Promise.all(cacheNames.map((cache) => { if (cache !== CACHE_NAME) return caches.delete(cache) })))
+    event.waitUntil(self.clients.claim())
+})
+
+self.addEventListener("fetch", event => {
+    event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)))
+})
